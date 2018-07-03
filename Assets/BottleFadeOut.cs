edf302acc8ee061b288c8b9c8c1f6136d48cbe73
furkaninalcik <m.furkaninalcik@gmail.com>
2018-07-03 15:13:33 +0300

    initial commit

---
+++ Assets/BottleFadeOut.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleFadeOut : MonoBehaviour {
+
+	public Animator fadeOutAnimaton;
+
+	public void startFading(){
+
+		print("START FADING");
+		fadeOutAnimaton.enabled = true;	
+
+	}
+
+
+
+}
---
+++ Assets/ItemCollection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollection : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	private void Update () {
+		if(Input.GetMouseButtonDown(0)){
+
+			print("TEST1");
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit, 100.0f)){
+				if(hit.transform != null ){
+
+					PrintName(hit.transform.gameObject);
+					print("TEST2");
+
+					BottleFadeOut BFO = (BottleFadeOut)FindObjectOfType(typeof(BottleFadeOut));
+				    if (BFO){
+		        	    Debug.Log("BottleFadeOut object found: " + BFO.name);
+				    
+		        	    // FADEOUT ACTION
+		        	    BFO.startFading();
+
+
+
+				    }
+		        	else
+		            	Debug.Log("No BottleFadeOut object could be found");
+
+				}
+
+			}
+
+
+
+		}
+
+    }
+
+    private void PrintName(GameObject go){
+    	print(go.name);
+    }
+
+}
