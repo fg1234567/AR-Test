c585aad2635ae16b976d2ad019673da573d500da
furkaninalcik <m.furkaninalcik@gmail.com>
2018-07-04 14:34:37 +0300

    firebase realtime database added, android ndk is missing

--- Assets/ItemCollection.cs
+++ Assets/ItemCollection.cs
@@ -1,10 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.IO;
 
 public class ItemCollection : MonoBehaviour {
 
-	
+		// Use this for initialization
+
+	DatabaseReference reference;
+	string path;
+	string jsonString;
+
+	void Start () {
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+		  var dependencyStatus = task.Result;
+		  if (dependencyStatus == Firebase.DependencyStatus.Available) {
+		    // Set a flag here indiciating that Firebase is ready to use by your
+		    // application.
+		  	Debug.Log("FIREBASE IS READY!");
+
+
+		  } else {
+		    UnityEngine.Debug.LogError(System.String.Format(
+		      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+		    // Firebase Unity SDK is not safe to use here.
+		  	Debug.Log("FIREBASE IS NOT READY!");
+
+
+		  }
+		});
+
+
+		// Set up the Editor before calling into the realtime database.
+	    FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://ar-test-firebase.firebaseio.com/");
+
+	    // Get the root reference location of the database.
+	    reference = FirebaseDatabase.DefaultInstance.RootReference;		
+
+
+	    path = Application.dataPath + "/JsonData/items.json";
+	    jsonString = File.ReadAllText(path);
+	}
+
+
 	// Update is called once per frame
 	private void Update () {
 		if(Input.GetMouseButtonDown(0)){
@@ -27,7 +68,10 @@ public class ItemCollection : MonoBehaviour {
 		        	    // FADEOUT ACTION
 		        	    BFO.startFading();
 
+		        	    //ITEM COLLECTION ACTION ON FIREBASE REALTIME DATABASE
 
+
+		        	    reference.SetRawJsonValueAsync(jsonString);
 
 				    }
 		        	else
