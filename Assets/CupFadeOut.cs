031f239971cbe09083d610d916add6f6d10c203a
furkaninalcik <m.furkaninalcik@gmail.com>
2018-07-06 18:24:33 +0300

    added main menu second commit

---
+++ Assets/CupFadeOut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupFadeOut : MonoBehaviour {
+
+	public Animator fadeOutAnimaton;
+
+	public void startFading(){
+
+		print("Cup START FADING");
+		fadeOutAnimaton.enabled = true;	
+
+	}
+
+}
